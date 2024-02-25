3d0fb0cfc50c6977efa43a3893a79f4800ff3fb4
Melekhin <s.melekhin@bftcom.com>
2024-02-25 19:20:56 +0300

    init

---
+++ src/main/java/com/example/apisport/ApiSportApplication.java
@@ -0,0 +1,13 @@
+package com.example.apisport;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiSportApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiSportApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/apisport/ApiSportApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.apisport;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiSportApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
